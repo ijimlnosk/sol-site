cf4b3e2a41034776ca8f3eb70d6ebceb78930a9c
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-04-15 14:04:04 +0900

    env 수정

--- sol-page/src/libs/axios/axios-instance.js
+++ sol-page/src/libs/axios/axios-instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getSessionToken } from "../auth/storage-manager";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.evn.REACT_APP_PEA_BASE_URL,
+    baseURL: import.meta.env.VITE_REACT_APP_PEA_BASE_URL,
     headers: {
         "Content-Type": "application/json",
     },
